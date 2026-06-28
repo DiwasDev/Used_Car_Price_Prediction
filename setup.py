7cdf73098a66540d6bb810b60884c8b35fca2d87
DivasDev <diwas.devs@gmail.com>
2026-06-28 11:29:47 +0000

    Fixed errors: requiremnets.txt and steup.py updated

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-# from setuptools import setup, find_packages
+from setuptools import setup, find_packages
 
-# setup(
-#     name="src",
-#     version="0.0.1",
-#     author="Diwas Pathak",
-#     author_email="diwas.devs@gmail.com",
-#     packages=find_packages()
-# )
+setup(
+    name="src",
+    version="0.0.1",
+    author="Diwas Pathak",
+    author_email="diwas.devs@gmail.com",
+    packages=find_packages()
+)
